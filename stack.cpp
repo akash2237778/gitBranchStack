f46546ffef595b6b7e18b2aeca65b1f1842510a8
akash2237778 <akash2237778@gmail.com>
2019-03-27 12:34:59 +0530

    pop function declared

--- stack.cpp
+++ stack.cpp
@@ -6,7 +6,18 @@ class stack{
 		
 	public:
 	void push(){}
-	void pop(){}
+	void pop(){ if(top==NULL)
+    {
+        cout<<"stack is empty"<<endl;
+    }
+    else
+    {
+        cout<<"Element pop off is :"<<top->data<<endl;
+        temp1=top;
+        temp1=temp1->next;
+        delete(top);
+        top=temp1;}
+
 	void display(){}
 }
 
