41a7943d9b00a889f457b0be1cf19d4581eec69a
akash2237778 <akash2237778@gmail.com>
2019-03-27 12:33:17 +0530

    private variables declared

--- stack.cpp
+++ stack.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 class stack{
 	private:
+struct node {
 
+    int data;
+    struct node* next;
+};
+    public:
+    struct node* top = NULL;
+    struct node* temp = NULL;
+    struct node* temp1 = NULL;
 		
 	public:
 	void push(){}
