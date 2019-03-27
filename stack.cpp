5c554cfdb117eed1a6ef46d537b2a0eabcaeb17c
akash2237778 <akash2237778@gmail.com>
2019-03-27 12:29:59 +0530

    push function designed

--- stack.cpp
+++ stack.cpp
@@ -5,7 +5,20 @@ class stack{
 
 		
 	public:
-	void push(){}
+	void push(int d){
+		if(top==NULL)
+    {
+     top = new node;
+     top->next=NULL;
+     top->data=d;
+    }
+    else
+    {
+    temp=new node;
+    temp->data=d;
+    temp->next=top;
+    top=temp;
+    }}
 	void pop(){}
 	void display(){}
 }
